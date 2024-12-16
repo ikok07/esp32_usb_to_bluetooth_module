e6b92719987c8f8d38abae9bab9eba57a9beb9e9
Kaloyan Stefanov <kokmarok@gmail.com>
2024-12-16 21:07:31 +0200

    parse keyboard data correctly

--- main/usb_app/usb_app.c
+++ main/usb_app/usb_app.c
@@ -14,71 +14,6 @@
 #include "hid_usage_keyboard.h"
 #include "tasks_common.h"
 
-/* Main char symbol for ENTER key */
-#define KEYBOARD_ENTER_MAIN_CHAR    '\r'
-/* When set to 1 pressing ENTER will be extending with LineFeed during serial debug output */
-#define KEYBOARD_ENTER_LF_EXTEND    1
-
-const uint8_t keycode2ascii [57][2] = {
-    {0, 0}, /* HID_KEY_NO_PRESS        */
-    {0, 0}, /* HID_KEY_ROLLOVER        */
-    {0, 0}, /* HID_KEY_POST_FAIL       */
-    {0, 0}, /* HID_KEY_ERROR_UNDEFINED */
-    {'a', 'A'}, /* HID_KEY_A               */
-    {'b', 'B'}, /* HID_KEY_B               */
-    {'c', 'C'}, /* HID_KEY_C               */
-    {'d', 'D'}, /* HID_KEY_D               */
-    {'e', 'E'}, /* HID_KEY_E               */
-    {'f', 'F'}, /* HID_KEY_F               */
-    {'g', 'G'}, /* HID_KEY_G               */
-    {'h', 'H'}, /* HID_KEY_H               */
-    {'i', 'I'}, /* HID_KEY_I               */
-    {'j', 'J'}, /* HID_KEY_J               */
-    {'k', 'K'}, /* HID_KEY_K               */
-    {'l', 'L'}, /* HID_KEY_L               */
-    {'m', 'M'}, /* HID_KEY_M               */
-    {'n', 'N'}, /* HID_KEY_N               */
-    {'o', 'O'}, /* HID_KEY_O               */
-    {'p', 'P'}, /* HID_KEY_P               */
-    {'q', 'Q'}, /* HID_KEY_Q               */
-    {'r', 'R'}, /* HID_KEY_R               */
-    {'s', 'S'}, /* HID_KEY_S               */
-    {'t', 'T'}, /* HID_KEY_T               */
-    {'u', 'U'}, /* HID_KEY_U               */
-    {'v', 'V'}, /* HID_KEY_V               */
-    {'w', 'W'}, /* HID_KEY_W               */
-    {'x', 'X'}, /* HID_KEY_X               */
-    {'y', 'Y'}, /* HID_KEY_Y               */
-    {'z', 'Z'}, /* HID_KEY_Z               */
-    {'1', '!'}, /* HID_KEY_1               */
-    {'2', '@'}, /* HID_KEY_2               */
-    {'3', '#'}, /* HID_KEY_3               */
-    {'4', '$'}, /* HID_KEY_4               */
-    {'5', '%'}, /* HID_KEY_5               */
-    {'6', '^'}, /* HID_KEY_6               */
-    {'7', '&'}, /* HID_KEY_7               */
-    {'8', '*'}, /* HID_KEY_8               */
-    {'9', '('}, /* HID_KEY_9               */
-    {'0', ')'}, /* HID_KEY_0               */
-    {KEYBOARD_ENTER_MAIN_CHAR, KEYBOARD_ENTER_MAIN_CHAR}, /* HID_KEY_ENTER           */
-    {0, 0}, /* HID_KEY_ESC             */
-    {'\b', 0}, /* HID_KEY_DEL             */
-    {0, 0}, /* HID_KEY_TAB             */
-    {' ', ' '}, /* HID_KEY_SPACE           */
-    {'-', '_'}, /* HID_KEY_MINUS           */
-    {'=', '+'}, /* HID_KEY_EQUAL           */
-    {'[', '{'}, /* HID_KEY_OPEN_BRACKET    */
-    {']', '}'}, /* HID_KEY_CLOSE_BRACKET   */
-    {'\\', '|'}, /* HID_KEY_BACK_SLASH      */
-    {'\\', '|'}, /* HID_KEY_SHARP           */  // HOTFIX: for NonUS Keyboards repeat HID_KEY_BACK_SLASH
-    {';', ':'}, /* HID_KEY_COLON           */
-    {'\'', '"'}, /* HID_KEY_QUOTE           */
-    {'`', '~'}, /* HID_KEY_TILDE           */
-    {',', '<'}, /* HID_KEY_LESS            */
-    {'.', '>'}, /* HID_KEY_GREATER         */
-    {'/', '?'} /* HID_KEY_SLASH           */
-};
-
 static const char TAG[] = "usb_app";
 
 QueueHandle_t usb_app_event_queue = NULL;
@@ -95,6 +30,12 @@ static void daemon_task(void *args) {
 
     // Send signal that host is installed
     xTaskNotifyGive(notify_task_handle);
+
+    // wait until HID driver is initialized
+    if (!ulTaskNotifyTake(true, pdMS_TO_TICKS(USB_APP_HID_INIT_TIMEOUT_MS))) {
+        ESP_LOGE(TAG, "TIMEOUT | HID Driver was not initialized in time!");
+        esp_restart();
+    }
 
     while (1) {
         uint32_t event_flags;
@@ -113,11 +54,14 @@ static void daemon_task(void *args) {
                 ESP_LOGI(TAG, "Waiting for ALL_FREE Event...");
             }
         }
+
+        if (event_flags & USB_HOST_LIB_EVENT_FLAGS_ALL_FREE) {
+            ESP_LOGI(TAG, "All devices disconnected!");
+        }
     }
 
     ESP_LOGI(TAG, "No more USB Clients and Devices!");
     ESP_ERROR_CHECK(usb_host_uninstall());
-    vTaskSuspend(NULL);
 }
 
 static void hid_print_new_device_report_header(hid_protocol_t proto) {
@@ -193,16 +137,14 @@ static void hid_host_keyboard_report_callback(const uint8_t *const data, const s
             key_event.key_code = prev_keys[i];
             key_event.modifier = 0;
             key_event.state = KEY_STATE_RELEASED;
-            ESP_LOGI(TAG, "KEY RELEASED");
             key_event_callback(&key_event);
         }
 
         // Key has been pressed
-        if (prev_keys[i] > HID_KEY_ERROR_UNDEFINED && !key_found(prev_keys, kb_report->key[i], HID_KEYBOARD_KEY_MAX)) {
+        if (kb_report->key[i] > HID_KEY_ERROR_UNDEFINED && !key_found(prev_keys, kb_report->key[i], HID_KEYBOARD_KEY_MAX)) {
             key_event.key_code = kb_report->key[i];
             key_event.modifier = kb_report->modifier.val;
             key_event.state = KEY_STATE_PRESSED;
-            ESP_LOGI(TAG, "KEY PRESSED");
             key_event_callback(&key_event);
         }
     }
@@ -215,6 +157,7 @@ static void hid_host_interface_callback(
     const hid_host_interface_event_t event,
     void *arg)
 {
+    esp_err_t err;
     uint8_t data[64] = { 0 };
     size_t data_length = 0;
     hid_host_dev_params_t dev_params;
@@ -222,16 +165,19 @@ static void hid_host_interface_callback(
 
     switch (event) {
         case HID_HOST_INTERFACE_EVENT_INPUT_REPORT:
-            ESP_ERROR_CHECK(
-                hid_host_device_get_raw_input_report_data(hid_device_handle, data, 64, &data_length)
-                );
+            err = hid_host_device_get_raw_input_report_data(hid_device_handle, data, 64, &data_length);
+            if (err != ESP_OK) {
+                ESP_LOGE(TAG, "Failed to get raw report data from USB device!");
+                return;
+            }
 
             if (dev_params.sub_class == HID_SUBCLASS_BOOT_INTERFACE) {
                 if (dev_params.proto == HID_PROTOCOL_KEYBOARD) {
                     hid_host_keyboard_report_callback(data, data_length);
+                    // The bluetooth part...
                 }
             } else {
-                // TODO...
+                // TODO (mouse?)
             }
         break;
         case HID_HOST_INTERFACE_EVENT_DISCONNECTED:
@@ -268,7 +214,6 @@ static void hid_host_device_event(
 
             ESP_ERROR_CHECK(hid_host_device_open(hid_device_handle, &dev_config));
             if (dev_params.sub_class == HID_SUBCLASS_BOOT_INTERFACE) {
-                ESP_LOGI(TAG, "BOOT SUBCLASS");
                 hid_report_protocol_t protocol = HID_REPORT_PROTOCOL_REPORT;
                 ESP_ERROR_CHECK(hid_class_request_get_protocol(hid_device_handle, &protocol));
 
@@ -289,14 +234,14 @@ static void hid_host_device_callback(
     void *arg)
 {
     const usb_app_event_queue_t evt_queue = {
-        .event_group = APP_EVENT_HID_HOST,
+        .event_group = USB_APP_EVENT_HID_HOST,
         .hid_host_device = {
             .handle = hid_device_handle,
             .event = event,
             .arg = arg
         }
     };
-    ESP_LOGI(TAG, "DEVICE NEW");
+
     if (usb_app_event_queue) {
         xQueueSend(usb_app_event_queue, &evt_queue, 0);
     }
@@ -304,18 +249,28 @@ static void hid_host_device_callback(
 
 void usb_init() {
     TaskHandle_t daemon_task_handle = NULL;
+    usb_app_event_queue = xQueueCreate(10, sizeof(usb_app_event_queue_t));
 
     // Initialize daemon
-    xTaskCreatePinnedToCore(
+    const bool daemon_task_created = xTaskCreatePinnedToCore(
         daemon_task,
         "daemon_task",
         USB_APP_DEAMON_TASK_STACK_SIZE,
-        &daemon_task_handle,
+        xTaskGetCurrentTaskHandle(),
         USB_APP_DEAMON_TASK_PRIORITY,
         &daemon_task_handle,
         USB_APP_DEAMON_TASK_CORE_ID
     );
-    ulTaskNotifyTake(false, 1000);
+    if (!daemon_task_created) {
+        ESP_LOGE(TAG, "Failed to create USB Daemon task!");
+        esp_restart();
+    }
+
+    // wait until USB Host driver is initialized
+    if (!ulTaskNotifyTake(false, pdMS_TO_TICKS(USB_APP_HID_INIT_TIMEOUT_MS))) {
+        ESP_LOGE(TAG, "TIMEOUT | USB Host Driver was not initialized in time!");
+        esp_restart();
+    }
 
     const hid_host_driver_config_t hid_host_driver_config = {
         .create_background_task = true,
@@ -327,17 +282,18 @@ void usb_init() {
     };
 
     ESP_ERROR_CHECK(hid_host_install(&hid_host_driver_config));
-
-    usb_app_event_queue = xQueueCreate(10, sizeof(usb_app_event_queue_t));
+    xTaskNotifyGive(daemon_task_handle);
 
     usb_app_event_queue_t evt_queue;
     while (1) {
         if (xQueueReceive(usb_app_event_queue, &evt_queue, portMAX_DELAY)) {
-            if (evt_queue.event_group == APP_EVENT_HID_HOST) {
-                hid_host_device_event(
+            switch (evt_queue.event_group) {
+                case USB_APP_EVENT_HID_HOST:
+                    hid_host_device_event(
                     evt_queue.hid_host_device.handle,
                     evt_queue.hid_host_device.event,
                     evt_queue.hid_host_device.arg);
+                break;
             }
         }
     }
--- main/usb_app/usb_app.h
+++ main/usb_app/usb_app.h
@@ -10,21 +10,76 @@
 
 #include "hid_host.h"
 
-#define USB_APP_VBUS_GPIO                       21  // GPIO to monitor if usb is connected
+#define USB_APP_VBUS_GPIO                       21          // GPIO to monitor if usb is connected
+#define USB_APP_HID_INIT_TIMEOUT_MS             60000       // 60 seconds
 
-#define CLASS_DRIVER_ACTION_OPEN_DEV    0x01
-#define CLASS_DRIVER_ACTION_TRANSFER    0x02
-#define CLASS_DRIVER_ACTION_CLOSE_DEV   0x03
+/* Main char symbol for ENTER key */
+#define KEYBOARD_ENTER_MAIN_CHAR    '\r'
+/* When set to 1 pressing ENTER will be extending with LineFeed during serial debug output */
+#define KEYBOARD_ENTER_LF_EXTEND    1
 
-#define USB_HID_CLASS                   0x03            // The interface class for a keyboard is defined as HID (Human Interface Device).
-#define USB_BOOT_INTERFACE_SUBCLASS     0x01            // ypically set to 0x01 for keyboards or 0x00 for generic HID devices.
-#define USB_KEYBOARD_PROTOCOL           0x01            // Specifies that the interface is used for keyboard input.
-
-#define USB_BM_ATTRIBUTES_XFER_MASK 0x03                // Extracts the transfer type from the bmAttributes field of an endpoint descriptor.
-#define USB_EP_DIR_IN 0x80                              // Checks if the endpoint direction is IN (device-to-host).
+static const uint8_t keycode2ascii [57][2] = {
+    {0, 0}, /* HID_KEY_NO_PRESS        */
+    {0, 0}, /* HID_KEY_ROLLOVER        */
+    {0, 0}, /* HID_KEY_POST_FAIL       */
+    {0, 0}, /* HID_KEY_ERROR_UNDEFINED */
+    {'a', 'A'}, /* HID_KEY_A               */
+    {'b', 'B'}, /* HID_KEY_B               */
+    {'c', 'C'}, /* HID_KEY_C               */
+    {'d', 'D'}, /* HID_KEY_D               */
+    {'e', 'E'}, /* HID_KEY_E               */
+    {'f', 'F'}, /* HID_KEY_F               */
+    {'g', 'G'}, /* HID_KEY_G               */
+    {'h', 'H'}, /* HID_KEY_H               */
+    {'i', 'I'}, /* HID_KEY_I               */
+    {'j', 'J'}, /* HID_KEY_J               */
+    {'k', 'K'}, /* HID_KEY_K               */
+    {'l', 'L'}, /* HID_KEY_L               */
+    {'m', 'M'}, /* HID_KEY_M               */
+    {'n', 'N'}, /* HID_KEY_N               */
+    {'o', 'O'}, /* HID_KEY_O               */
+    {'p', 'P'}, /* HID_KEY_P               */
+    {'q', 'Q'}, /* HID_KEY_Q               */
+    {'r', 'R'}, /* HID_KEY_R               */
+    {'s', 'S'}, /* HID_KEY_S               */
+    {'t', 'T'}, /* HID_KEY_T               */
+    {'u', 'U'}, /* HID_KEY_U               */
+    {'v', 'V'}, /* HID_KEY_V               */
+    {'w', 'W'}, /* HID_KEY_W               */
+    {'x', 'X'}, /* HID_KEY_X               */
+    {'y', 'Y'}, /* HID_KEY_Y               */
+    {'z', 'Z'}, /* HID_KEY_Z               */
+    {'1', '!'}, /* HID_KEY_1               */
+    {'2', '@'}, /* HID_KEY_2               */
+    {'3', '#'}, /* HID_KEY_3               */
+    {'4', '$'}, /* HID_KEY_4               */
+    {'5', '%'}, /* HID_KEY_5               */
+    {'6', '^'}, /* HID_KEY_6               */
+    {'7', '&'}, /* HID_KEY_7               */
+    {'8', '*'}, /* HID_KEY_8               */
+    {'9', '('}, /* HID_KEY_9               */
+    {'0', ')'}, /* HID_KEY_0               */
+    {KEYBOARD_ENTER_MAIN_CHAR, KEYBOARD_ENTER_MAIN_CHAR}, /* HID_KEY_ENTER           */
+    {0, 0}, /* HID_KEY_ESC             */
+    {'\b', 0}, /* HID_KEY_DEL             */
+    {0, 0}, /* HID_KEY_TAB             */
+    {' ', ' '}, /* HID_KEY_SPACE           */
+    {'-', '_'}, /* HID_KEY_MINUS           */
+    {'=', '+'}, /* HID_KEY_EQUAL           */
+    {'[', '{'}, /* HID_KEY_OPEN_BRACKET    */
+    {']', '}'}, /* HID_KEY_CLOSE_BRACKET   */
+    {'\\', '|'}, /* HID_KEY_BACK_SLASH      */
+    {'\\', '|'}, /* HID_KEY_SHARP           */  // HOTFIX: for NonUS Keyboards repeat HID_KEY_BACK_SLASH
+    {';', ':'}, /* HID_KEY_COLON           */
+    {'\'', '"'}, /* HID_KEY_QUOTE           */
+    {'`', '~'}, /* HID_KEY_TILDE           */
+    {',', '<'}, /* HID_KEY_LESS            */
+    {'.', '>'}, /* HID_KEY_GREATER         */
+    {'/', '?'} /* HID_KEY_SLASH           */
+};
 
 typedef enum {
-    APP_EVENT_HID_HOST = 0
+    USB_APP_EVENT_HID_HOST = 0
 } usb_app_event_group_e;
 
 typedef struct {
